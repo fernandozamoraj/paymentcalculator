9ff62651bb5b6480187e822a03ff398e42edec0d
Fernando Zamora <fernando.zamora.jimenez@gmail.com>
2013-03-23 03:11:52 -0500

    added additional columns for payment for principal and payment towards interest as well as some validation... I will release the next version once I put some contact information

--- payment calculator/calculator.js
+++ payment calculator/calculator.js
@@ -1,21 +1,43 @@
 
-                        
+                        //http://snipplr.com/view/46696/
+                        //This jquery extension forces a numeric value
+						(function ($) {
+							$.fn.forceNumeric = function () {
+                                return this.each(function () {
+ 
+                                    $(this).keyup(function() {
+                                        if (!/^[0-9]+$/.test($(this).val())) {
+                                           $(this).val($(this).val().replace(/[^0-9]/g, ''));
+                                        }
+                                    });
+                                });
+                            };
+                        })(jQuery);
             
                          function registerEvents(){
                             
                             $('#calculate-payment-button').click( function(){
                                 calculatePayment(); 
                             });
+							
+							$('#principal').forceNumeric();
+							$('#interest-rate').forceNumeric();
+							$('#term').forceNumeric();
+														
 						  }
                            
                              var calculatePayment = function(){
                              
-                                var principal = $('#principal').val() * 1;
+                                var principal = ($('#principal').val()).replace(',', '') * 1;
                                 var interestRate = $('#interest-rate').val() * 1;
                                 var termInMonths = $('#term').val() * 1;
-                                
+								
+								interestRate = interestRate <= 0 ? 0 : interestRate;
+								principal <= 0 ? 0 : principal;
+								termInMonths <= 0 ? 1 : termInMonths;
+								
                                 var monthlyInterest = interestRate/(12*100);
-                                var monthlyPayment = principal * (monthlyInterest/(1-Math.pow(1 + monthlyInterest, -termInMonths)));
+                                var monthlyPayment = monthlyInterest <= 0 ? principal / termInMonths : principal * (monthlyInterest/(1-Math.pow(1 + monthlyInterest, -termInMonths)));
                                 
                                 $('#calculated-payment').html("Your Payment: $" + addCommas(monthlyPayment.toFixed(2)));
                                 $('#amortization-schedule').html("");
@@ -47,21 +69,60 @@
                                 
                             });
 							
+							function inputIsValid(){
+							    
+								var isValid = true;
+								var principal = $('#principal').val();
+                                var interestRate = $('#interest-rate').val();
+                                var termInMonths = $('#term').val();
+
+								$('#principal').css('border-color', 'white');
+								$('#interest-rate').css('border-color', 'white');
+								$('#term').css('border-color', 'white');
+								
+                                if(principal <= 0 || !/^[0-9,]+$/.test($('#principal').val())){
+								    setFieldInvalid('#principal');
+                                    isValid = false;
+							    }
+								if(interestRate < 0 || !/^[0-9.]+$/.test($('#interest-rate').val())){
+                                    setFieldInvalid('#interest-rate');
+                                    isValid = false;
+								}
+								if(termInMonths <= 0 || !/^[0-9]+$/.test($('#term').val())){
+                                    setFieldInvalid('#term');
+                                    isValid = false;								
+								}
+								
+								return isValid;							
+							}
+							
+							function setFieldInvalid(txtBox){
+							    $(txtBox).css('border-color', 'red');
+								
+							}
+							
 							function handleClick(){
-							                                var schedule = "";
-                                var principal = $('#principal').val() * 1;       //multiply times 1 to force to numeric value --- otherwise it will be text
+							    
+								if(inputIsValid() === false)
+									return;
+								
+								var schedule = "";
+                                var principal = ($('#principal').val()).replace(',', '') * 1;       //multiply times 1 to force to numeric value --- otherwise it will be text
                                 var interestRate = $('#interest-rate').val() * 1;
                                 var termInMonths = $('#term').val() * 1;
+								
+								termInMonths = termInMonths <= 0 ? 1 : termInMonths;
                                 var originalPrincipal = principal;
+                                var monthlyInterest = interestRate <= 0 ? 0 : interestRate/(12*100);
+								var monthlyPayment = interestRate <= 0 ? principal / termInMonths :  principal * (monthlyInterest/(1-Math.pow(1 + monthlyInterest, -termInMonths)));
                                 
-                                var monthlyInterest = interestRate/(12*100);
-                                var monthlyPayment = principal * (monthlyInterest/(1-Math.pow(1 + monthlyInterest, -termInMonths)));
-                                
+								
+								
                                 var totalInterest = 0.0;
                                 
                                 calculatePayment();
                                 
-                                schedule = "<table><tr><th>Payment</th><th>Principal</th><th class='exclude-on-mobile'>Accumulated Interest</th></tr>";
+                                schedule = "<table><tr><th>Payment</th><th>Principal</th><th class='exclude-on-mobile'>Accumulated Interest</th><th class='exclude-on-mobile'>Principal Payment</th><th class='exclude-on-mobile'>Interest Payment</th></tr>";
                                 
                                 console.log("Monthly Interest: " + monthlyInterest);
                                 console.log("Principal: " + principal);
@@ -84,6 +145,10 @@
                                     schedule = schedule + "    <td>" + i +"</td>";
                                     schedule = schedule + "    <td>$ " + addCommas(principal.toFixed(2)) + "</td>";
                                     schedule = schedule + "    <td class='exclude-on-mobile'>$ " + addCommas(totalInterest.toFixed(2)) + "</td>";
+									schedule = schedule + "    <td class='exclude-on-mobile'>$ " + addCommas((monthlyPayment-currentInterest).toFixed(2)) + "</td>";
+									schedule = schedule + "    <td class='exclude-on-mobile'>$ " + addCommas(currentInterest.toFixed(2)) + "</td>";
+									
+									
                                     schedule = schedule + "</tr>";
                                     
                                     if(principal <= 0)
